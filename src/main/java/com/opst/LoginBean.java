1b054d65f90cd61cdd0e1b27685e6d2e1fb79d69
Marton Koran <koran.mrci@gmail.com>
2015-08-04 05:05:13 +0100

    changover to shiro-faces from homebrewed

--- src/main/java/com/opst/LoginBean.java
+++ src/main/java/com/opst/LoginBean.java
@@ -28,10 +28,15 @@ public class LoginBean implements Serializable{
   
 
   public String logout() {
-      currentUser.logout();
+      if (currentUser.isAuthenticated()){
+          currentUser.logout();
     System.out.println("[INFO] - LOGINBEAN: Logging "+getRemoteUser()+" out.");
+    return "/mcdst/index?faces-redirect=true";
+      }
+      else {  
          
     return "/mcdst/index?faces-redirect=true";
+      }
   }
     public String getRemoteUser(){
        if (currentUser.isAuthenticated()){
