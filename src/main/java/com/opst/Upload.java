e1c95a0ef6f7f5b87bbff7fe1f97e7dc655a297a
Marton Koran <koran.mrci@gmail.com>
2015-08-15 20:45:12 +0100

    a new start

--- src/main/java/com/opst/Upload.java
+++ src/main/java/com/opst/Upload.java
@@ -151,8 +151,8 @@ public class Upload extends HttpServlet {
         java.sql.Date sqlDate = new java.sql.Date(date.getTime());
         stmt = conn.createStatement();
         // the mysql insert statement
-      String query = " insert into ctp_daily (date, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, avg)"
-        + " values (?, ?, ?, ?, ?,?, ?, ?, ?, ?,?, ?, ?, ?, ?,?, ?, ?, ?, ?,?, ?, ?, ?, ?,?)";
+      String query = " insert into `ctp_daily` (`date`, `1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`, `10`, `11`, `12`, `13`, `14`, `15`, `16`, `17`, `18`, `19`, `20`, `21`, `22`, `23`, `24`, `avg`)"
+        + " values (?, ?, ?, ?, ?,?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
  
       // create the mysql insert preparedstatement
       PreparedStatement preparedStmt = conn.prepareStatement(query);
