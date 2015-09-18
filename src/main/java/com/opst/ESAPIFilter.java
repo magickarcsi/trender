abbf76cc05feb7caa488fd3eb1991d7986ff7e02
Marton Koran <koran.mrci@gmail.com>
2015-09-18 21:13:44 +0100

    authenticate

--- src/main/java/com/opst/ESAPIFilter.java
+++ src/main/java/com/opst/ESAPIFilter.java
@@ -38,90 +38,33 @@ public class ESAPIFilter implements Filter {
 
 	private static final String[] obfuscate = { "password" };
 
-	/**
-	 * Called by the web container to indicate to a filter that it is being
-	 * placed into service. The servlet container calls the init method exactly
-	 * once after instantiating the filter. The init method must complete
-	 * successfully before the filter is asked to do any filtering work.
-	 * 
-	 * @param filterConfig
-	 *            configuration object
-	 */
-
-	/**
-	 * The doFilter method of the Filter is called by the container each time a
-	 * request/response pair is passed through the chain due to a client request
-	 * for a resource at the end of the chain. The FilterChain passed in to this
-	 * method allows the Filter to pass on the request and response to the next
-	 * entity in the chain.
-	 * 
-	 * @param req
-	 *            Request object to be processed
-	 * @param resp
-	 *            Response object
-	 * @param chain
-	 *            current FilterChain
-	 * @exception IOException
-	 *                if any occurs
-     * @throws javax.servlet.ServletException
-	 */
+	
         @Override
 	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
 		HttpServletRequest request = (HttpServletRequest) req;
 		HttpServletResponse response = (HttpServletResponse) resp;
-		ESAPI.httpUtilities().setCurrentHTTP(request, response);
+                try {
+                    ESAPI.authenticator().login(request, response);
+                }
+                catch( AuthenticationException e ){
+                ESAPI.authenticator().logout();
+                                request.setAttribute("message", "Authentication failed");
+                                RequestDispatcher dispatcher = request.getRequestDispatcher("login.xhtml");
+                                dispatcher.forward(request, response);
+                }
+                chain.doFilter(request, response);
 
-        if (!(request instanceof HttpServletRequest)) {
-            chain.doFilter(request, response);
-            return;
+                
         }
-        try {
-			// figure out who the current user is
-			try {
-				ESAPI.authenticator().login(request, response);
-			} catch( AuthenticationException e ) {
-				ESAPI.authenticator().logout();
-				request.setAttribute("message", "Authentication failed");
-				RequestDispatcher dispatcher = request.getRequestDispatcher("login.xhtml");
-				dispatcher.forward(request, response);
-				return;
-			}
-
-			// log this request, obfuscating any parameter named password
-			ESAPI.httpUtilities().logHTTPRequest(request, logger, Arrays.asList(obfuscate));
-
-			// check access to this URL
-			if ( !ESAPI.accessController().isAuthorizedForURL(request.getRequestURI().toString()) ) {
-				request.setAttribute("message", "Unauthorized" );
-				RequestDispatcher dispatcher = request.getRequestDispatcher("login.xhtml");
-				dispatcher.forward(request, response);
-				return;
-			}
-			
-			
-		} finally {
-			// VERY IMPORTANT
-			// clear out the ThreadLocal variables in the authenticator
-			// some containers could possibly reuse this thread without clearing the User
-			ESAPI.authenticator().clearCurrent();
-			ESAPI.httpUtilities().setCurrentHTTP(null, null);
-		}
-        }
-
-	/**
-	 * Called by the web container to indicate to a filter that it is being
-	 * taken out of service. This method is only called once all threads within
-	 * the filter's doFilter method have exited or after a timeout period has
-	 * passed. After the web container calls this method, it will not call the
-	 * doFilter method again on this instance of the filter.
-	 */
-	public void destroy() {
-		// finalize
-	}
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    @Override
+    public void destroy() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
+		
